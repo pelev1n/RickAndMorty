0b13e65d605d947504bba21dddb53ab85ae98f11
pelev1n <pelevin9911@gmail.com>
2018-09-02 16:47:32 +0300

    Added logic to LocationsFragment

--- app/src/main/java/com/andrewxa/rickandmorty/view/fragments/LocationsFragment.java
+++ app/src/main/java/com/andrewxa/rickandmorty/view/fragments/LocationsFragment.java
@@ -1,4 +1,33 @@
 package com.andrewxa.rickandmorty.view.fragments;
 
-public class LocationsFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andrewxa.rickandmorty.R;
+
+public class LocationsFragment extends Fragment{
+
+    public LocationsFragment() {
+
+    }
+
+    public static LocationsFragment newInstance() {
+        LocationsFragment fragment = new LocationsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_list_extend_item, container, false);
+
+        return view;
+    }
 }
