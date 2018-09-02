2ebd481835368e1898c53c3edf31a95dcf070a79
pelev1n <pelevin9911@gmail.com>
2018-09-02 16:46:43 +0300

    Added logic to EpisodesFragment

--- app/src/main/java/com/andrewxa/rickandmorty/view/fragments/EpisodesFragment.java
+++ app/src/main/java/com/andrewxa/rickandmorty/view/fragments/EpisodesFragment.java
@@ -1,4 +1,33 @@
 package com.andrewxa.rickandmorty.view.fragments;
 
-public class EpisodesFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andrewxa.rickandmorty.R;
+
+public class EpisodesFragment extends Fragment{
+
+    public EpisodesFragment() {
+
+    }
+
+    public static EpisodesFragment newInstance() {
+        EpisodesFragment fragment = new EpisodesFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_list_extend_item, container, false);
+
+        return view;
+    }
 }
