a8ce0fb8df9da92dad5a221ad13526476483961f
pelev1n <pelevin9911@gmail.com>
2018-09-03 15:59:52 +0300

    Added handler of location get response

--- app/src/main/java/com/andrewxa/rickandmorty/datasource/network/RickAndMortyApi.java
+++ app/src/main/java/com/andrewxa/rickandmorty/datasource/network/RickAndMortyApi.java
@@ -1,6 +1,7 @@
 package com.andrewxa.rickandmorty.datasource.network;
 
 import com.andrewxa.rickandmorty.datasource.model.characters.CharactersInfo;
+import com.andrewxa.rickandmorty.datasource.model.locations.LocationsInfo;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,6 +10,9 @@ public interface RickAndMortyApi {
 
     @GET("character")
     Call<CharactersInfo> getAllCharachters();
+
+    @GET("location")
+    Call<LocationsInfo> getAllLocations();
 
 
 }
--- app/src/main/java/com/andrewxa/rickandmorty/presenter/LocationsPresenter.java
+++ app/src/main/java/com/andrewxa/rickandmorty/presenter/LocationsPresenter.java
@@ -3,16 +3,14 @@ package com.andrewxa.rickandmorty.presenter;
 import android.content.Context;
 
 import com.andrewxa.rickandmorty.RickAndMortyApp;
-import com.andrewxa.rickandmorty.datasource.contract.Contract;
-import com.andrewxa.rickandmorty.datasource.model.characters.CharactersInfo;
-import com.andrewxa.rickandmorty.view.fragments.characters.CharactersFragment;
+import com.andrewxa.rickandmorty.datasource.model.locations.LocationsInfo;
 import com.andrewxa.rickandmorty.view.fragments.locations.Locations;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LocationsPresenter {
+public class LocationsPresenter{
     Context mContext;
     Locations view;
 
@@ -22,15 +20,15 @@ public class LocationsPresenter {
     }
 
     public void requestAllLocations() {
-        Call<CharactersInfo> charachters = RickAndMortyApp.getRickAndMortyApi().getAllCharachters();
-        charachters.enqueue(new Callback<CharactersInfo>() {
+        Call<LocationsInfo> locations = RickAndMortyApp.getRickAndMortyApi().getAllLocations();
+        locations.enqueue(new Callback<LocationsInfo>() {
             @Override
-            public void onResponse(Call<CharactersInfo> call, Response<CharactersInfo> response) {
+            public void onResponse(Call<LocationsInfo> call, Response<LocationsInfo> response) {
                 view.showAllLocations(response.body().getResults());
             }
 
             @Override
-            public void onFailure(Call<CharactersInfo> call, Throwable t) {
+            public void onFailure(Call<LocationsInfo> call, Throwable t) {
 
             }
         });
