ad21957e6e148874eab542166e19ac59c420a720
pelev1n <pelevin9911@gmail.com>
2018-09-03 15:46:44 +0300

    Changed logic of LocationsFragment

--- app/src/main/java/com/andrewxa/rickandmorty/view/fragments/locations/LocationsFragment.java
+++ app/src/main/java/com/andrewxa/rickandmorty/view/fragments/locations/LocationsFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.andrewxa.rickandmorty.R;
+import com.andrewxa.rickandmorty.presenter.CharactersPresenter;
+import com.andrewxa.rickandmorty.presenter.LocationsPresenter;
 
 public class LocationsFragment extends Fragment implements Locations{
+
+    RecyclerView recyclerView;
+    LocationsPresenter locationsPresenter;
 
     public LocationsFragment() {
 
@@ -26,8 +32,11 @@ public class LocationsFragment extends Fragment implements Locations{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.characters_list, container, false);
+        View view = inflater.inflate(R.layout.locations_fragment, container, false);
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.locations_list);
+        locationsPresenter = new CharactersPresenter(this,getContext());
+        getAllCharacters();
 
         return view;
     }
